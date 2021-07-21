597c8b61a5455e586ed3d588414fedc3d5bcb3c4
Coreen Cooper <coreencooper@pursuit.org>
2021-07-20 21:20:02 -0400

    update apiURL

--- front-end/src/util/apiURL.js
+++ front-end/src/util/apiURL.js
@@ -1,8 +1,5 @@
 export const apiURL = () => {
-  return "https://young-peak-76026.herokuapp.com"
-
-  // return window.location.hostname === "localhost"
-  //   ? "http://localhost:3333"
-  //   : "https://young-peak-76026.herokuapp.com";
-
+  return window.location.hostname === "localhost"
+    ? "http://localhost:3333"
+    : "https://young-peak-76026.herokuapp.com/";
 };
