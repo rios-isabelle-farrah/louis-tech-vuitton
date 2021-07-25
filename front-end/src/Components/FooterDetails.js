21969b30d44e42f2ac913a05b21e7ec9b8180570
Carlo <carlonyc001@gmail.com>
2021-07-25 19:03:45 -0400

    footer details

--- front-end/src/Components/FooterDetails.js
+++ front-end/src/Components/FooterDetails.js
@@ -8,7 +8,7 @@ function FooterDetails() {
         <div className="footer-brand"> <img src={Logo}></img></div>
         <div className="footer-links">
             <a href="https://github.com/rios-isabelle-farrah/louis-tech-vuitton" target="_blank">Github</a>
-            <a href="">Heroku</a>
+            <a href="https://young-peak-76026.herokuapp.com/" target="_blank">Heroku</a>
             <a className="link-name" href="https://github.com/rios-isabelle-farrah" target="_blank">Farrah Rios</a>
             <a className="link-name" href="https://github.com/CoreenCooper" target="_blank">Coreen Cooper</a>
             <a className="link-name" href="https://github.com/bedonjonathan" target="_blank">Jonathan Bedon</a>
