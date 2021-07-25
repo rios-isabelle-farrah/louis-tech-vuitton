6772b7c7b7ea2f11d72ff8dedc5fea7527efcb4e
Coreen Cooper <coreencooper@pursuit.org>
2021-07-25 14:09:51 -0400

    update seed.sql

--- front-end/src/Components/UserListItem.js
+++ front-end/src/Components/UserListItem.js
@@ -1,9 +1,10 @@
 function UserListItem({ usernames }) {
-  console.log(usernames);
+//   console.log(usernames);
 //   console.log(usernames[usernames.length - 1]["username"]);
+// const currentUser = usernames[usernames.length - 1]["username"];
 
   return <h2 className="user-details">
-      Welcome {usernames[usernames.length - 1].username} !
+      {/* {currentUser ?  currentUser : null} */}
       </h2>;
 }
 
