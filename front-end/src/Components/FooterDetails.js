b282b3e5d8d1b86ab318979c8a23452650bd04d1
Carlo <carlonyc001@gmail.com>
2021-07-25 16:34:08 -0400

    _blank complete

--- front-end/src/Components/FooterDetails.js
+++ front-end/src/Components/FooterDetails.js
@@ -9,10 +9,10 @@ function FooterDetails() {
         <div className="footer-links">
             <a href="https://github.com/rios-isabelle-farrah/louis-tech-vuitton" target="_blank">Github</a>
             <a href="">Heroku</a>
-            <a className="link-name" href="https://github.com/rios-isabelle-farrah">Farrah Rios</a>
-            <a className="link-name" href="https://github.com/CoreenCooper">Coreen Cooper</a>
-            <a className="link-name" href="https://github.com/bedonjonathan">Jonathan Bedon</a>
-            <a className="link-name" href="https://github.com/carloecheverri">Carlo Echeverri</a>
+            <a className="link-name" href="https://github.com/rios-isabelle-farrah" target="_blank">Farrah Rios</a>
+            <a className="link-name" href="https://github.com/CoreenCooper" target="_blank">Coreen Cooper</a>
+            <a className="link-name" href="https://github.com/bedonjonathan" target="_blank">Jonathan Bedon</a>
+            <a className="link-name" href="https://github.com/carloecheverri" target="_blank">Carlo Echeverri</a>
         </div>
         <div>
           <span> &copy; {new Date().getFullYear()} creativeFour. </span>
